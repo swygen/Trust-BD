34c888d9e4db7bc50d9cc6e6877aee322754d42f
swygen <swygenofficial@gmail.com>
2025-06-09 22:37:21 +0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+// src/App.js
+import React from "react";
+import AuthPage from "./AuthPage";
+import "@fontsource/poppins";
+import "@fontsource/material-icons";
+
+function App() {
+  return <AuthPage />;
+}
+
+export default App;
